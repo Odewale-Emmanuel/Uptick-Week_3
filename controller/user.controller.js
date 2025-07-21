6fd71c4632f2f81c3cd71357925b61d7531497dd
odewale emmanuel <odebzemmygee@gmail.com>
2025-07-21 16:48:39 +0100

    get user by id (query param) request added

---
+++ controller/user.controller.js
@@ -0,0 +1,29 @@
+const { where, Op } = require("sequelize");
+const db = require("../models/index");
+
+async function getUser(req, res, next) {
+  try {
+    const { id } = req.query;
+    const user = await db.user.findAll({
+      where: {
+        id: {
+          [Op.eq]: id,
+        },
+      },
+    });
+
+    if (!user.length) {
+      return res.status(200).send(`Sorry user with id: ${id} not found!`);
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "An error occurred while trying to get requested user. please ensure you send a valid user id"
+      );
+  }
+}
+
+module.exports = getUser;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 // const createHttpError = require("http-errors");
 const cookieParser = require("cookie-parser");
 // const db = require("./models/index.js");
+const userRouter = require("./routes/user.route");
 const usersRouter = require("./routes/users.route");
 const notesRouter = require("./routes/notes.route.js");
 const createUserRouter = require("./routes/user.create.route.js");
@@ -23,6 +24,7 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.use("/user", userRouter);
 app.use("/users", usersRouter);
 app.use("/notes", notesRouter);
 app.use("/create-user", createUserRouter);
